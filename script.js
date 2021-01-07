1b3320f1a6eeb55b4bd82b82b497b17475f5f64b
perrycq <perrycq@gmail.com>
2021-01-07 16:14:37 -0600

    Added click button for ask button via jquery. Have not linked up return answer.

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-var answers = ["yes", "no", "maybe", "try again later", "without a doubt", "Never in a million years"];
-var question = prompt ("What would you like to know?");
-var randomNumber = Math.random();
-var randomIndex = randomNumber * answers.length;
-var roundedIndex = Math.floor(randomIndex);
-var pickedAnswer = answers[roundedIndex];
-var magic8Ball = {};
-magic8Ball.answeredQuestion = function (question, pickedAnswer){
-console.log("Question: " + question); 
-console.log("Answer: " + pickedAnswer);	
-};
+$(document).ready(function() {
 
-magic8Ball.answeredQuestion(question, pickedAnswer);
+    var magic8Ball = {};
+    var answers = ["yes", "no", "maybe", "try again later", "without a doubt", "Never in a million years"];
+    var randomNumber = Math.random();
+    var randomIndex = randomNumber * answers.length;
+    var roundedIndex = Math.floor(randomIndex);
+    var pickedAnswer = answers[roundedIndex];
+    var answerReturn = document.getElementById("answer");
+    
+    var buttonPressed = function() {
+        var question = prompt ("What would you like to know?");
+        answerReturn.innerText(pickedAnswer);
+    };
+    
+    $("#questionButton").click(buttonPressed);
+    
+    });
