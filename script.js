9b134aa6e7153af5bdd472dd5846363656d6e669
perrycq <perrycq@gmail.com>
2021-01-08 15:54:02 -0600

    Switched bits of code to be JQuery, added functions and list of answers to be properties of magic8Ball object. Verify answers update per question.

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
 
     var magic8Ball = {};
-    var answers = ["yes", "no", "maybe", "try again later", "without a doubt", "Never in a million years"];
-    var randomNumber = Math.random();
-    var randomIndex = randomNumber * answers.length;
-    var roundedIndex = Math.floor(randomIndex);
-    var pickedAnswer = answers[roundedIndex];
-    var answerReturn = document.getElementById("answer");
+    magic8Ball.listofAnswers = ["yes", "no", "maybe", "try again later", "without a doubt", "Never in a million years"];
+
+    magic8Ball.askQuestion = function(question){
+        var randomNumber = Math.random();
+        var randomIndex = randomNumber * this.listofAnswers.length;
+        var roundedIndex = Math.floor(randomIndex);
+        var pickedAnswer = this.listofAnswers[roundedIndex];
+    
+        $("#answer").text(pickedAnswer);
+
+        console.log(question);
+        console.log(answer);
+    };
+    
     
     var buttonPressed = function() {
-        var question = prompt ("What would you like to know?");
-        answerReturn.innerText = pickedAnswer;
+        var question = prompt ("What would you like to know? (Ask a yes/no question)");
+        magic8Ball.askQuestion(question);
     };
     
     $("#questionButton").click(buttonPressed);
