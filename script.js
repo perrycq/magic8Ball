9caeb4a38caa28897638579365d47a3a1d2c2d37
perrycq <perrycq@gmail.com>
2021-01-08 20:25:31 -0600

    Added special effects with jquery to magic 8 ball.

--- script.js
+++ script.js
@@ -4,21 +4,31 @@ $(document).ready(function() {
     magic8Ball.listofAnswers = ["yes", "no", "maybe", "try again later", "without a doubt", "Never in a million years"];
 
     magic8Ball.askQuestion = function(question){
+        $("#answer").fadeIn(4000);
+        $("#8ball").attr("src","https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+        
         var randomNumber = Math.random();
         var randomIndex = randomNumber * this.listofAnswers.length;
         var roundedIndex = Math.floor(randomIndex);
         var pickedAnswer = this.listofAnswers[roundedIndex];
-    
+
         $("#answer").text(pickedAnswer);
 
         console.log(question);
-        console.log(answer);
+        console.log(pickedAnswer);
     };
-    
+
+    $("#answer").hide();
     
     var buttonPressed = function() {
+        $("#answer").hide();
+        $("#8ball").attr("src","https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/8side.png");
+        setTimeout(
+            function() {
         var question = prompt ("What would you like to know? (Ask a yes/no question)");
+        $("#8ball").effect("shake");
         magic8Ball.askQuestion(question);
+    }, 500);
     };
     
     $("#questionButton").click(buttonPressed);
